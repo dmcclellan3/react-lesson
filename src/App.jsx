d1df507bd3319324b55f654d931b393717c43778
dmcclellan3 <wdmcclellan3@gmail.com>
2024-05-28 16:22:49 -0400

    able to create users

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
+import { useContext } from "react"
+import { fetchUser } from "./api"
+import { AuthContext } from "./authContext"
+
 
 
 function App() {
+
+  const { auth } = useContext(AuthContext)
+
+  const submit = () => {
+    fetchUser({ auth })
+  }
+
   return (
     <div className="p-5">
       <h1>Home</h1>
+      <button onClick={() => submit()}>Fetch Profile</button>
     </div>
   )
 }
--- src/api.js
+++ src/api.js
@@ -1,14 +1,46 @@
 import axios from 'axios'
 
-const baseUrl = 'http://127.0.0.1:8000/'
+const baseUrl = 'http://127.0.0.1:8000'
+
+export const createUser = ({ username, password, firstName, lastName }) => {
+    axios({
+        method: 'post',
+        url: `${baseUrl}/create-user/`,
+        data: {
+            username,
+            password,
+            first_name: firstName, 
+            last_name: lastName,
+        }
+    })
+    .then(response => {
+        console.log('CREATE USER RESPONSE: ', response)
+    })
+    .catch(error => console.log('ERROR: ', error))
+}
+
 
 export const getToken = ({ auth, username, password }) => {
+    console.log('GET TOKEN: ', auth, username, password)
     axios.post(`${baseUrl}/token/`, {
         username,
         password,
     }) .then(response => {
+        console.log('GET TOKEN RESPONSE: ', response)
         auth.setAccessToken(response.data.access)
     })
     .catch(error => console.log('Error: ', error))
 }
 
+
+export const fetchUser = ({ auth }) => {
+    axios({
+        method: 'get',
+        url:`${baseUrl}/profile/`,
+        headers: {
+            Authorization: `Bearer ${auth.access}`
+        }
+    }).then(response => {
+        console.log('FETCH USER RESPONSE: ', response)
+    }).catch(error => console.log('ERROR: ', error))
+}
